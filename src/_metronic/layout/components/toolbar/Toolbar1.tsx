4d3ae900c01735882206e6029edd331ecafa842f
Edmond <oleg.livnenko.freelancer@gmail.com>
2022-04-24 05:48:08 +0300

    update action from filter/create to search

--- src/_metronic/layout/components/toolbar/Toolbar1.tsx
+++ src/_metronic/layout/components/toolbar/Toolbar1.tsx
@@ -19,39 +19,14 @@ const Toolbar1: FC = () => {
 
         {/* begin::Actions */}
         <div className='d-flex align-items-center py-1'>
-          {/* begin::Wrapper */}
-          <div className='me-4'>
-            {/* begin::Menu */}
-            <a
-              href='#'
-              className='btn btn-sm btn-flex btn-light btn-active-primary fw-bolder'
-              data-kt-menu-trigger='click'
-              data-kt-menu-placement='bottom-end'
-              data-kt-menu-flip='top-end'
-            >
-              <KTSVG
-                path='/media/icons/duotune/general/gen031.svg'
-                className='svg-icon-5 svg-icon-gray-500 me-1'
-              />
-              Filter
-            </a>
-
-            {/* end::Menu */}
+          <div className="input-group">
+            <div className="form-outline">
+              <input placeholder='search' type="search" id="form1" className="form-control" />
+            </div>
+            <button type="button" className="btn btn-primary">
+              <i className="fas fa-search"></i>
+            </button>
           </div>
-          {/* end::Wrapper */}
-
-          {/* begin::Button */}
-
-          <a
-            href='#'
-            className='btn btn-sm btn-primary'
-            data-bs-toggle='modal'
-            data-bs-target='#kt_modal_create_app'
-            id='kt_toolbar_primary_button'
-          >
-            Create
-          </a>
-          {/* end::Button */}
         </div>
         {/* end::Actions */}
       </div>
