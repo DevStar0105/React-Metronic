b71a6c280239572f1de88ba168f9d637d16fbbce
Edmond <oleg.livnenko.freelancer@gmail.com>
2022-04-24 03:50:44 +0300

    change aside menu main and add icon

--- src/_metronic/layout/components/aside/AsideMenuMain.tsx
+++ src/_metronic/layout/components/aside/AsideMenuMain.tsx
@@ -11,6 +11,58 @@ export function AsideMenuMain() {
   return (
     <>
       <AsideMenuItem
+        to='/ventures'
+        icon='/media/icons/duotune/art/cube.svg'
+        title='Ventures'
+        fontIcon='bi-app-indicator'
+      />
+      <AsideMenuItem
+        to='/explore'
+        icon='/media/icons/duotune/art/compass.svg'
+        title='Explore'
+        fontIcon='bi-app-indicator'
+      />
+      <AsideMenuItem
+        to='/favorites'
+        icon='/media/icons/duotune/art/heart.svg'
+        title='Favorites'
+        fontIcon='bi-app-indicator'
+      />
+      <AsideMenuItem
+        to='/myventures'
+        icon='/media/icons/duotune/art/bookmark.svg'
+        title='My ventures'
+        fontIcon='bi-app-indicator'
+      />
+      <AsideMenuItemWithSub
+        to='/crafted/accounts'
+        title='Nests'
+        icon='/media/icons/duotune/art/layers.svg'
+        fontIcon='bi-person'
+      >
+        <AsideMenuItem to='/nests/all' title='All Nests' hasBullet={false} />
+        <AsideMenuItem to='/nests/my' title='My Nests' hasBullet={false} />
+        <AsideMenuItem to='/nests/pending' title='Pending Acceptance' hasBullet={false} />
+      </AsideMenuItemWithSub>
+      <AsideMenuItem
+        to='/profile'
+        icon='/media/icons/duotune/art/profile.svg'
+        title='Profile'
+        fontIcon='bi-app-indicator'
+      />
+      <AsideMenuItem
+        to='/createven'
+        icon='/media/icons/duotune/art/plus.svg'
+        title='Create Venture'
+        fontIcon='bi-app-indicator'
+      />
+      <AsideMenuItem
+        to='/faq'
+        icon='/media/icons/duotune/art/help.svg'
+        title='FAQ'
+        fontIcon='bi-layers'
+      />
+      <AsideMenuItem
         to='/dashboard'
         icon='/media/icons/duotune/art/art002.svg'
         title={intl.formatMessage({id: 'MENU.DASHBOARD'})}
